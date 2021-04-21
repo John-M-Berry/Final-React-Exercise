98a26250c5f76a59de7aa483a3c2680393f25e1d
MjrKong42 <mjr.kong42@gmail.com>
2021-04-21 09:24:34 -0500

    Main exercise complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+// 2a. Open the App.js file and add the necessary import line at the top of the file that allows the MCUShows component to be used in the App.js file
 import MCUShows from './mcu-shows/MCUShows';
+// 5a. Below the MCUShows import in the App.js file, create the following variable:
+// const releaseDates = {
+//  wandaVision: 'Jan 2021',
+//  falconWinter: 'Mar 2021',
+//  loki: 'June 2021',
+//  hawkeye: 'Fall of 2021'
+// }
 const releaseDates = {
- wandaVision: 'Jan 2021',
- falconWinter: 'Mar 2021',
- loki: 'June 2021',
- hawkeye: 'Fall of 2021'
+  wandaVision: 'Jan 2021',
+  falconWinter: 'Mar 2021',
+  loki: 'June 2021',
+  hawkeye: 'Fall of 2021'
 }
 function App() {
   return (
+    // 2b. Inside the div in the App component, create an h1 with the following text: "FINAL REACT EXERCISE"
     <div>
-<h1>FINAL REACT EXERCISE</h1>
-<MCUShows />
+      <h1>FINAL REACT EXERCISE</h1>
+      {/* // 2c. Below the h1 pass in a MCUShows component */}
+      
+      {/* // 5b. Add a prop called "dates" to the MCUShows component that is being passed into the App.js component. Also, set the releaseDates variable as the value that is passed into the "dates" prop */}
+
+      <MCUShows dates={releaseDates} />
     </div>
   );
 }
--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -2,7 +2,8 @@
 import './MCUShows.css';
 // 1b. Create a function component using an arrow function expression called MCUShows
 
-const MCUShows = () => {
+// 5c. Go back to the MCUShows.js file. Plug the values from the dates property, which holds the value of the releaseDates object, in each p element after each appropriate show name. (HINT: The properties created for a component are stored in an object that can be accessed via a parameter in a function component)
+const MCUShows = ({dates}) => {
     // 1c. Inside the MCUShows function/component, use the RETURN keyword with parentheses to have the MCUShows function/component return the following elements inside of a single div: h1, h2, and 4 p (paragraph) elements. Put the following text in each element. (NOTE: Notice the colon and space after each show name)
     return (
             // h1: "Disney+"
@@ -12,18 +13,18 @@ const MCUShows = () => {
             // p element 3: "Loki: "
             // p element 4: "Hawkeye: "
 
-            // 3a. Go back to the MCUShows.js file. Using inline styling in React, Set the font (A.K.A. font family) for the h1 to the "Georgia" font. Also, add an underline to the h1 (HINT: Use the Text Decoration property)
-
+            
             // 4a. Add a class name of "MCUShows" to the the div in the MCUShows component
-        <div className='MCUShows'>
-            <h1 style={{fontFamily: 'Georgia', textDecoration: 'undeline'}}>Disney+</h1>;
-            <h2>MCU Show Release Dates</h2>;
-            <p>WandaVision: </p>
-            <p>The Falcon and the Winter Soldier: </p>
-            <p>Loki: </p>
-            <p>Hawkeye: </p>
+            <div className='MCUShows'>
+            {/* // 3a. Go back to the MCUShows.js file. Using inline styling in React, Set the font (A.K.A. font family) for the h1 to the "Georgia" font. Also, add an underline to the h1 (HINT: Use the Text Decoration property) */}
+            <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
+            <h2>MCU Show Release Dates</h2>
+      {/* 5c */}
+      <p>WandaVision: {dates.wandaVision}</p>
+      <p>The Falcon and the Winter Soldier: {dates.falconWinter}</p>
+      <p>Loki: {dates.loki}</p>
+      <p>Hawkeye: {dates.hawkeye}</p>
         </div>
-
     );
 }
 // 1d. Add the export line at the bottom of the file
